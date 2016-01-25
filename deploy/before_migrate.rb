474c93ab103322bdc651acef7ddaf4cffb5c56b8
EC2 Default User <ec2-user@ip-10-0-1-118.ap-northeast-1.compute.internal>
2016-01-25 05:19:50 +0000

    5th

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,4 @@
 Chef::Log.info("before_migrate.rb executing")
 application = new_resource.params[:app]
 deploy = new_resource.params[:deploy_data]
+Chef::Log.info("USER_ID: #{node[:deploy]['app1'][:environment_variables][:USER_ID]}")
